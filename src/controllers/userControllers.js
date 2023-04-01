703d4807c15926a205082c291695da0cd1c6d44a
breno-aredes <breno_mg10@hotmail.com>
2023-04-01 13:41:53 -0300

    feat: Added error handling middleware and libraries
    Changes made:
    1 - Added errorMiddleware.js file to handle application errors in the middleware folder
    2 - Added app.use(handleApplicationErrors) in server.js to use the error middleware
    
    3 - Installed http-status and express-async-errors libraries to handle errors
    
    4 - Implemented proper error handling in userService.js file
    
    5 - Added next parameter to catch block in userControllers.js file to handle errors properly.

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,24 +1,24 @@
 import userServices from "../services/userServices.js";
 
-async function signup(req, res) {
+async function signup(req, res, next) {
   const { name, email, password, confirmPassword, type } = req.body;
 
   try {
     await userServices.signup({ name, email, password, confirmPassword, type });
     return res.sendStatus(201);
   } catch (error) {
-    return res.status(500).send(error.message);
+    next(error);
   }
 }
 
-async function signin(req, res) {
+async function signin(req, res, next) {
   const { email, password } = req.body;
 
   try {
     const token = await userServices.signin({ email, password });
     return res.send({ token });
   } catch (error) {
-    return res.status(500).send(error.message);
+    next(error);
   }
 }
 
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,13 +1,15 @@
 import bcrypt from "bcrypt";
 import userRepositories from "../repositories/userRepositories.js";
 import jwt from "jsonwebtoken";
+import errors from "../errors/index.js";
 
 async function signup({ name, email, password, confirmPassword, type }) {
   const { rowCount } = await userRepositories.findByEmail(email);
 
-  if (password !== confirmPassword) throw new Error(`Passwords do not match`); //400
+  if (password !== confirmPassword)
+    throw errors.conflictError(`Passwords do not match`);
 
-  if (rowCount) throw new Error(`E-mail already registered`); //409
+  if (rowCount) throw errors.conflictError(`E-mail already registered`);
 
   const hashPassword = await bcrypt.hash(password, 10);
   await userRepositories.signup({ name, email, password: hashPassword, type });
@@ -19,11 +21,12 @@ async function signin({ email, password }) {
     rows: [user],
   } = await userRepositories.findByEmail(email);
 
-  if (!rowCount) throw new Error(`Incorrect e-mail or password`);
+  if (!rowCount) throw errors.conflictError(`Incorrect e-mail or password`);
 
   const validPassword = await bcrypt.compare(password, user.password);
 
-  if (!validPassword) throw new Error(`Incorrect e-mail or password`);
+  if (!validPassword)
+    throw errors.conflictError(`Incorrect e-mail or password`);
 
   const dataUser = {
     id: user.id,
